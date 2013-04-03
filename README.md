c8d0136f7d4ffbcd306c1eb9f5739a854a20245d
garbray <darks@dksource.com.ar>
2013-04-02 23:56:11 -0500

    latest deploy for the conference

---
+++ README.md
@@ -0,0 +1,3 @@
+#Ejemplos del segundo BogotaJS
+
+[Presentacion de la conferencia](http://www.rvl.io/garbray/aplicaciones-con-amd-y-requirejs)
---
+++ example-2/js/build.js
@@ -0,0 +1,14 @@
+({
+    baseUrl: ".",
+    //rutas de las dependencias
+    paths: {
+        jquery: "libs/jquery.min",
+        underscore: "libs/underscore-min",
+        backbone: "libs/backbone-min",
+        bootstrap: "libs/bootstrap.min"
+    },
+    //nombre del modulo principal
+    name: "main",
+    //modulo resultante
+    out: "main-built.js"
+})
